f3e18f78347282e598c9cbf0cebbb648dccb3d88
yovelsapir <yovelsapir@gmail.com>
2019-10-16 17:30:33 +0300

    Updates

---
+++ precache-manifest.9b659f08c4c09cdd1cefe9774fd65ad2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09c69fea6be6a2ed12f7fde7ca6e0cf7",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "cc4f784332821c062b09",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "27a15cbf80e864b51707",
+    "url": "/robofriends/static/css/main.2f2b48f8.chunk.css"
+  },
+  {
+    "revision": "cc4f784332821c062b09",
+    "url": "/robofriends/static/js/2.4566c3fe.chunk.js"
+  },
+  {
+    "revision": "27a15cbf80e864b51707",
+    "url": "/robofriends/static/js/main.7ae5661f.chunk.js"
+  },
+  {
+    "revision": "ef7ed2c3e67d53841831",
+    "url": "/robofriends/static/js/runtime-main.ed74653d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
